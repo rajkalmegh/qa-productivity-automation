c9333895ec17b990d66b897279e378f8c6884c69
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-04 18:20:48 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,20 @@
 
+async function uploadFile(){
+
+let file = document.getElementById("fileInput").files[0]
+
+let formData = new FormData()
+
+formData.append("file", file)
+
+let response = await fetch("/generate-report", {
+    method: "POST",
+    body: formData
+})
+
+let data = await response.json()
+
+document.getElementById("result").innerText =
+"Report Generated in " + data.execution_time_seconds + " seconds"
+
+}
