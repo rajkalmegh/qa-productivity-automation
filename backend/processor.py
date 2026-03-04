35a6cf5f33a983ed5b08aaf2dc0ab934bc60dd7f
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-04 18:19:38 +0530

    Update processor.py

--- backend/processor.py
+++ backend/processor.py
@@ -1,1 +1,18 @@
 
+import pandas as pd
+
+def process_data(file):
+
+    df = pd.read_csv(file)
+
+    # Tester productivity
+    tester_summary = df.groupby("Name").size().reset_index(name="Total Bugs")
+
+    # Severity summary
+    severity_summary = df.groupby("Severity").size().reset_index(name="Count")
+
+    # Product summary
+    product_summary = df.groupby("Product").size().reset_index(name="Count")
+
+    return tester_summary, severity_summary, product_summary
+  
