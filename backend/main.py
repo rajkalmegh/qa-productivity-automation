821fb134780d32727102b7c0ad68c897e77759f2
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-04 18:19:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,32 @@
 
+from fastapi import FastAPI, UploadFile, File
+from processor import process_data
+from report_generator import generate_report
+import time
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "QA Productivity Automation Running"}
+
+@app.post("/generate-report")
+async def generate(file: UploadFile = File(...)):
+
+    start = time.time()
+
+    tester_summary, severity_summary, product_summary = process_data(file.file)
+
+    report_path = generate_report(
+        tester_summary,
+        severity_summary,
+        product_summary
+    )
+
+    execution_time = round(time.time() - start, 2)
+
+    return {
+        "status": "success",
+        "report": report_path,
+        "execution_time_seconds": execution_time
+    }
