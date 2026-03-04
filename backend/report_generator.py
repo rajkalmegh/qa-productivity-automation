cc84532f9cc7a12bd37cd0c2cbf3b57e1404f461
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-04 18:19:54 +0530

    Update report_generator.py

--- backend/report_generator.py
+++ backend/report_generator.py
@@ -1,1 +1,29 @@
 
+import pandas as pd
+
+def generate_report(tester_summary, severity_summary, product_summary):
+
+    report_file = "daily_productivity_report.xlsx"
+
+    with pd.ExcelWriter(report_file) as writer:
+
+        tester_summary.to_excel(
+            writer,
+            sheet_name="Tester Productivity",
+            index=False
+        )
+
+        severity_summary.to_excel(
+            writer,
+            sheet_name="Severity Summary",
+            index=False
+        )
+
+        product_summary.to_excel(
+            writer,
+            sheet_name="Product Summary",
+            index=False
+        )
+
+    return report_file
+  
