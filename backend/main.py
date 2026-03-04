a22e173683a230b5b460b3f42af56ae88fc7048f
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-04 18:29:09 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,32 +1,32 @@
-
 from fastapi import FastAPI, UploadFile, File
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from processor import process_data
 from report_generator import generate_report
 import time
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
 @app.get("/")
-def home():
-    return {"message": "QA Productivity Automation Running"}
+def dashboard():
+    return FileResponse("frontend/index.html")
+
 
 @app.post("/generate-report")
 async def generate(file: UploadFile = File(...)):
 
     start = time.time()
 
-    tester_summary, severity_summary, product_summary = process_data(file.file)
+    tester, severity, product = process_data(file.file)
 
-    report_path = generate_report(
-        tester_summary,
-        severity_summary,
-        product_summary
-    )
+    report = generate_report(tester, severity, product)
 
-    execution_time = round(time.time() - start, 2)
+    execution_time = round(time.time() - start,2)
 
     return {
-        "status": "success",
-        "report": report_path,
-        "execution_time_seconds": execution_time
+        "message":"Report Generated",
+        "execution_time":execution_time,
+        "report":report
     }
