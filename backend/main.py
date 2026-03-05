8ef9f961b9fdfb729027a7c56c6d7006d5ca56eb
RUTURAJ G.KALMEGH <74487940+rajkalmegh@users.noreply.github.com>
2026-03-05 15:58:23 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,39 +1,37 @@
 from fastapi import FastAPI
+import time
+from backend.devops_fetcher import fetch_devops_data
 from backend.processor import process_data
 from backend.report_generator import generate_report
-from backend.devops_fetcher import fetch_devops_data
-import time
 
 app = FastAPI()
-
 
 @app.get("/")
 def home():
     return {"message": "QA Productivity Automation Running"}
 
-
 @app.post("/generate-report")
 def generate_report_api():
+    try:
+        start = time.time()
 
-    start = time.time()
+        df = fetch_devops_data()
 
-    # Fetch bugs directly from Azure DevOps
-    df = fetch_devops_data()
+        tester_summary, severity_summary, product_summary = process_data(df)
 
-    # Process productivity data
-    tester_summary, severity_summary, product_summary = process_data(df)
+        report_path = generate_report(
+            tester_summary,
+            severity_summary,
+            product_summary
+        )
 
-    # Generate Excel report
-    report_path = generate_report(
-        tester_summary,
-        severity_summary,
-        product_summary
-    )
+        execution_time = round(time.time() - start, 2)
 
-    execution_time = round(time.time() - start, 2)
+        return {
+            "status": "success",
+            "report": report_path,
+            "execution_time_seconds": execution_time
+        }
 
-    return {
-        "status": "success",
-        "report": report_path,
-        "execution_time_seconds": execution_time
-    }
+    except Exception as e:
+        return {"error": str(e)}
